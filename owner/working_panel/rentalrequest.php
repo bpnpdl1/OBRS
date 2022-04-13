e9bca54ed6ab16fc037b0d5ad3f2ec3de1f6a2e1
Bpnpdl <bipo_bca2076@lict.edu.np>
2022-04-13 07:46:38 +0545

    Some changes made in rent search in owner module

--- owner/working_panel/rentalrequest.php
+++ owner/working_panel/rentalrequest.php
@@ -21,8 +21,8 @@ require_once '../header.php';
 
     <div class="mx-3 d-flex justify-content-between">
         <h3>Rent Request</h3>
-        <div class="form-group d-flex justify-content-center w-9">
-
+        <div class="form-group d-flex justify-content-end w-9">
+            <input type="text" name="ticket" placeholder="Search Ticket" class="form-control">
 
         </div>
         <a href="http://localhost/Online_Bike_Rental_System/owner/working_panel">Go back</a>
